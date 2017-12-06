ef6cc0ba3143cb430f5b553be47a63b6a65ec114
Nicholas Wiersma <nick@wiersma.co.za>
2017-12-06 08:17:59 +0100

    Gracefully shutdown the http server

--- cmd/transporter/server.go
+++ cmd/transporter/server.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/msales/transporter"
 	"github.com/msales/transporter/server"
@@ -24,9 +29,23 @@ func runServer(c *cli.Context) {
 	defer app.Close()
 
 	port := c.String(FlagPort)
-	s := newServer(ctx, app)
-	ctx.logger.Info(fmt.Sprintf("Starting server on port %s", port))
-	log.Fatal(http.ListenAndServe(":"+port, s))
+	srv := newServer(ctx, app)
+	h := http.Server{Addr: ":" + port, Handler: srv}
+	defer func() {
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+		h.Shutdown(ctx)
+	}()
+	go func() {
+		ctx.logger.Info(fmt.Sprintf("Starting server on port %s", port))
+		if err := h.ListenAndServe(); err != nil {
+			if err != http.ErrServerClosed {
+				log.Fatal(err)
+			}
+		}
+	}()
+
+	quit := listenForSignals()
+	<-quit
 }
 
 func newServer(ctx *Context, app *transporter.Application) http.Handler {
@@ -34,4 +53,20 @@ func newServer(ctx *Context, app *transporter.Application) http.Handler {
 
 	h := middleware.Common(s)
 	return middleware.WithContext(ctx, h)
+}
+
+// Wait for SIGTERM to end the application.
+func listenForSignals() chan bool {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigs
+
+		done <- true
+	}()
+
+	return done
 }
